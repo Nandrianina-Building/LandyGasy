e860305bf1e869db175bbff1423eec20a52dff6b
Nandrianina-Building <nandrianinabuilding@gmail.com>
2025-05-06 12:09:59 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+const search = document.querySelector(".search");
+const SearchSection = document.querySelector(".searchCard");
+
+search.addEventListener("click",  function(){
+    alert("aaa")
+    SearchSection.classList.toggle("active");
+})
